d4f819916a98d09f210c77f260eb1cb880781193
Hovo-Dev <hovhannes.baghdasaryan.03@gmail.com>
2023-09-20 16:45:35 +0400

    finish end-to-end console

--- event_loop/script.js
+++ event_loop/script.js
@@ -1,19 +1,14 @@
 import { CodeBlocks } from "./const.js";
-
-// const delayInput = document.querySelector('.delay')
-//
-// const inputHandler = function(e) {
-//     console.log(e.target.value)
-//     delayInput.value = e.target.value.replace(/[^\d.-]+/g, '')
-// }
-//
-// delayInput.addEventListener("change", inputHandler);
-//
+import { initCallStack } from "./utils.js";
 
 const menu = document.querySelector(".menu")
 const codeEditor = document.querySelector(".codeEditor")
 const run = document.getElementById("run")
+const callbackElement = document.getElementById("callback_element")
+const event_spinner = document.getElementById("event_spinner")
+
 const codeBlocks = [];
+const eventLoopBlocks = []
 
 menu.addEventListener('click', (e) => {
     const {name} = e.target.dataset
@@ -30,16 +25,38 @@ menu.addEventListener('click', (e) => {
 run.addEventListener('click', () => {
     let code = ``;
 
+    callbackElement.innerHTML = ""
+    eventLoopBlocks.length = 0
+    
     codeBlocks.map(block => {
         switch (block.type) {
             case "console":
                 code += `\n console.log("${block.consoleValue()}")`
+                const newConsoleItem = {
+                    element: {
+                        view: `console.log("${block.consoleValue()}")`,
+                        callback:`console.log("${block.consoleValue()}")`,
+                    }, 
+                    type: 'console'
+                }
+                eventLoopBlocks.push(newConsoleItem)
                 break;
 
             case "timeout":
-                code += `\n setTimeout(() => {
-                    console.log("${block.consoleValue()}")
+                const newTimeoutView = `\n setTimeout(() => {
+                    console.log("${block.consoleValue()}") 
                 }, ${Number(block.timeoutValue())})`
+
+                code += newTimeoutView
+                const newTimeoutItem = {
+                    element: {
+                        callback:`console.log("${block.consoleValue()}")`,
+                        delay: Number(block.timeoutValue()),
+                        view: newTimeoutView,
+                    }, 
+                    type: 'timeout'
+                }
+                eventLoopBlocks.push(newTimeoutItem)
                 break;
 
             case "interval":
@@ -60,6 +77,17 @@ run.addEventListener('click', () => {
         }
     })
 
-    const execute = new Function(code)
-    execute()
+    const addCallStack = initCallStack(eventLoopBlocks, callbackElement)
+
+    addCallStack()
+
+    setInterval(() => {
+        const callback = addCallStack()
+
+        if(!callback) clearInterval(callback)
+    }, 2000)
 })
+
+
+
+// event_spinner.classList.add('rotate')
--- event_loop/utils.js
+++ event_loop/utils.js
@@ -8,4 +8,34 @@ export const defineId = () => {
    }
 }
 
+export const initCallStack = (eventLoopBlocks, callbackElement) => {
+    let index = 0
+    
+    return () => {
+        if(index === eventLoopBlocks.length) return false
+
+        const newCallStackItem = document.createElement("div")
+
+        newCallStackItem.classList.add('callstack-item')
+        
+        newCallStackItem.innerHTML = eventLoopBlocks[index].element.view
+        
+        callbackElement.appendChild(newCallStackItem)
+
+        new Function(eventLoopBlocks[index].element.callback)()
+
+        setTimeout(() => {
+            newCallStackItem.classList.add('callstack-item-remove')
+
+            setTimeout(() => {
+                newCallStackItem.classList.remove('callstack-item')
+                newCallStackItem.classList.remove('callstack-item-remove')
+                callbackElement.removeChild(newCallStackItem)
+            }, 1000)
+
+            index++
+        }, 1000)
+    }
+}
+
 export const getInputValue = (id) => () => document.getElementById(id).value
