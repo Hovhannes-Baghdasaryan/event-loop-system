a12692421a6c4a4a53049f4c4e70aa21d40e3999
hrach347 <80620675+hrach347@users.noreply.github.com>
2023-09-21 18:22:35 +0400

    v8 tokenizing demo

--- v8js/app.js
+++ v8js/app.js
@@ -9,14 +9,16 @@ run.addEventListener('click', () => {
 })
 let stack = []
 let spans = []
+let NeedsEnd = 0
+
 function convertCodeToSpans() {
     let codeLines = code.value.split('\n')
     let str = code.value
-    // code.value = ""
     code.style.display = "none"
     fakeCode.style.display = "block"
     for (let i = 0; i < codeLines.length; i++) {
-        let line = codeLines[i]
+        let line = codeLines[i].trimStart()
+        line = line.trimEnd()
         for (let j = 0; j < line.length; j++) {
             let line1 = line.split(' ')
             if (line.includes('//')) {
@@ -24,7 +26,7 @@ function convertCodeToSpans() {
                 span.classList.add('comment')
                 span.innerText = codeLines[i]
                 span.style.display = 'block'
-                spans.push({text: "Comment Meta",el: span})
+                spans.push({ text: "Comment Meta", el: span })
                 fakeCode.append(span)
                 break
             }
@@ -34,25 +36,25 @@ function convertCodeToSpans() {
                 let span = document.createElement('span')
                 span.classList.add('let')
                 span.innerText = "let "
-                spans.push({text: "Declaration Meta",el: span})
+                spans.push({ text: "Declaration Meta", el: span })
                 bigSpan.append(span)
                 span = document.createElement('span')
                 span.classList.add('varName')
                 span.innerText = line1[1] + " "
-                spans.push({text: "Name Meta",el: span})
+                spans.push({ text: "Name Meta", el: span })
                 bigSpan.append(span)
                 if (line1[2] == "=") {
                     span = document.createElement('span')
                     span.classList.add('white')
                     span.innerText = line1[2] + " "
-                    spans.push({text: "Equal Meta",el: span})
+                    spans.push({ text: "Equal Meta", el: span })
                     bigSpan.append(span)
                 }
                 if ((!line1[3].startsWith('"') && !line1[3].endsWith('"')) && isNaN(Number(line1[3])) && !line1[3].startsWith('[') && !line1[3].startsWith('{')) {
                     span = document.createElement('span')
                     span.classList.add('varName')
                     span.innerText = line1[3]
-                    spans.push({text: "Value Meta",el: span})
+                    spans.push({ text: "Value Meta", el: span })
                     bigSpan.append(span)
                     stack.push({
                         name: line1[1],
@@ -64,7 +66,7 @@ function convertCodeToSpans() {
                     span = document.createElement('span')
                     span.classList.add('varValue')
                     span.innerText = value[1]
-                    spans.push({text: "Value Meta",el: span})
+                    spans.push({ text: "Value Meta", el: span })
                     bigSpan.append(span)
                     stack.push({
                         name: line1[1],
@@ -80,24 +82,117 @@ function convertCodeToSpans() {
                 let span = document.createElement('span')
                 span.classList.add('varName')
                 span.innerText = "console."
-                spans.push({text: "Console Meta",el: span})
+                spans.push({ text: "Console Meta", el: span })
                 let span2 = document.createElement('span')
                 span2.classList.add('foo')
-                span2.innerText = "log("
-                spans.push({text: "Log Meta",el: span2})
+                span2.innerText = "log"
+                spans.push({ text: "Log Meta", el: span2 })
                 let value = line.split("console.log(")
                 let span3 = document.createElement('span')
                 if (!value[1].startsWith('"') && isNaN(Number(value[1])) && !value[1].startsWith('[') && !value[1].startsWith('{')) {
-                span3.classList.add('varName')
+                    span3.classList.add('varName')
                 }
-                else{
-                span3.classList.add('varValue')
+                else {
+                    span3.classList.add('varValue')
                 }
-                span3.innerText = value[1]
-                spans.push({text: "Argument Meta",el: span3})
+                span3.innerText = "(" + value[1]
+                spans.push({ text: "Argument Meta", el: span3 })
                 bigSpan.append(span)
                 bigSpan.append(span2)
                 bigSpan.append(span3)
+                fakeCode.append(bigSpan)
+                break
+            }
+            else if ((line.startsWith('else if(') && typeof codeLines[i - 1] !== "undefined" && codeLines[i - 1] == "}")
+                &&
+                ((line.trimEnd().endsWith('{') && line.trimEnd()[line.length - 2] == ")")
+                    ||
+                    (line.endsWith(')') && typeof codeLines[i + 1] !== "undefined" && codeLines[i + 1].trimStart().startsWith('{'))
+                )
+            ) {
+                NeedsEnd++
+                let bigSpan = document.createElement('span')
+                bigSpan.style.display = "block"
+                let span = document.createElement('span')
+                span.classList.add('if')
+                span.innerText = "else if"
+                spans.push({ text: "Else If Meta", el: span })
+                let statement = line.split('else if')[1].split('{')[0]
+                let span2 = document.createElement('span')
+                span2.classList.add('varName')
+                span2.innerText = statement
+                spans.push({ text: "Condition Meta", el: span2 })
+                let span3 = document.createElement('span')
+                span3.classList.add('foo')
+                span3.innerText = "{"
+                spans.push({ text: "Opening Closure Meta", el: span3 })
+                bigSpan.append(span)
+                bigSpan.append(span2)
+                bigSpan.append(span3)
+                fakeCode.append(bigSpan)
+                break
+            }
+            else if (
+                line.startsWith('if(') && (line.endsWith('{') && line[line.length - 2] == ")")
+                ||
+                (line.endsWith(')') && typeof codeLines[i + 1] !== "undefined" && codeLines[i + 1].trimStart().startsWith('{'))
+            ) {
+                line = line.trimEnd()
+                NeedsEnd++
+                let bigSpan = document.createElement('span')
+                bigSpan.style.display = "block"
+                let span = document.createElement('span')
+                span.classList.add('if')
+                span.innerText = "if"
+                spans.push({ text: "If Meta", el: span })
+                let statement = line.split('if')[1].split('{')[0]
+                let span2 = document.createElement('span')
+                span2.classList.add('varName')
+                span2.innerText = statement
+                spans.push({ text: "Condition Meta", el: span2 })
+                let span3 = document.createElement('span')
+                span3.classList.add('foo')
+                span3.innerText = "{"
+                spans.push({ text: "Opening Closure Meta", el: span3 })
+                bigSpan.append(span)
+                bigSpan.append(span2)
+                bigSpan.append(span3)
+                fakeCode.append(bigSpan)
+                break
+            }
+            else if (
+                line.startsWith('while(') && (line.endsWith(')') && typeof codeLines[i + 1] !== "undefined"
+                    && codeLines[i + 1].trimStart().startsWith('{'))
+                || (line.startsWith('while(') && line.endsWith('{') && line[line.length - 2] == ")")
+            ) {
+                NeedsEnd++
+                let bigSpan = document.createElement('span')
+                bigSpan.style.display = "block"
+                let span = document.createElement('span')
+                span.classList.add('if')
+                span.innerText = "while"
+                spans.push({ text: "While Loop Meta", el: span })
+                let span2 = document.createElement('span')
+                span2.classList.add('varName')
+                spans.push({ text: "Condition Meta", el: span2 })
+                span2.innerText = line.split('while')[1].split('{')[0]
+                let span3 = document.createElement('span')
+                span3.classList.add('foo')
+                span3.innerText = "{"
+                spans.push({ text: "Opening Closure meta", el: span3 })
+                bigSpan.append(span)
+                bigSpan.append(span2)
+                bigSpan.append(span3)
+                fakeCode.append(bigSpan)
+                break
+            }
+            else if (line.trimStart().trimEnd() == "}" && NeedsEnd > 0) {
+                NeedsEnd--
+                let bigSpan = document.createElement('span')
+                bigSpan.style.display = "block"
+                bigSpan.classList.add("foo")
+                bigSpan.innerText = "}"
+                spans.push({ text: "Closing Closure Meta", el: bigSpan })
                 fakeCode.append(bigSpan)
                 break
             }
@@ -105,22 +200,21 @@ function convertCodeToSpans() {
     }
     console.log(stack)
     let i = 0
-    setTimeout(()=>{
+    setTimeout(() => {
         steps[0].style.opacity = 1
 
-    },2000)
-    let tokenizing = setInterval(()=>{
-        spans[i].el.style.opacity="0"
+    }, 2000)
+    let tokenizing = setInterval(() => {
+        spans[i].el.style.opacity = "0"
         spans[i].el.style.fontSize = "25px"
-        setTimeout(()=>{
-            spans[i].el.innerText = spans[i].text+" "
-            spans[i].el.style.opacity="1"
+        setTimeout(() => {
+            spans[i].el.innerText = spans[i].text + " "
+            spans[i].el.style.opacity = "1"
             i++
-        },1500)     
-        if(i>=spans.length-1){
-            steps[0].style.opacity="0.3"
+        }, 500)
+        if (i >= spans.length - 1) {
+            steps[0].style.opacity = "0.3"
             clearInterval(tokenizing)
         }
-    },3000)
+    }, 1000)
 }
-
